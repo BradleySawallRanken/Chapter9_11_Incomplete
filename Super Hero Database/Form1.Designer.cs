13add36ed8587c2a880dea91b284415c06c6775e
B_Sawall <Bradley_Sawall@insideranken.org>
2024-03-03 22:40:35 -0600

    First commit

---
+++ Super Hero Database/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Super_Hero_Database
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblLikes = new System.Windows.Forms.Label();
+            this.lblDislikes = new System.Windows.Forms.Label();
+            this.lblPower = new System.Windows.Forms.Label();
+            this.lblBio = new System.Windows.Forms.Label();
+            this.lblBiography = new System.Windows.Forms.Label();
+            this.lblWiki = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Thanos",
+            "Knull",
+            "Arnim Zola",
+            "Mister Negative",
+            "Yondu"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(337, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(413, 404);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(12, 52);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(14, 16);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "_";
+            // 
+            // lblLikes
+            // 
+            this.lblLikes.AutoSize = true;
+            this.lblLikes.Location = new System.Drawing.Point(12, 80);
+            this.lblLikes.Name = "lblLikes";
+            this.lblLikes.Size = new System.Drawing.Size(44, 16);
+            this.lblLikes.TabIndex = 3;
+            this.lblLikes.Text = "label2";
+            // 
+            // lblDislikes
+            // 
+            this.lblDislikes.AutoSize = true;
+            this.lblDislikes.Location = new System.Drawing.Point(12, 111);
+            this.lblDislikes.Name = "lblDislikes";
+            this.lblDislikes.Size = new System.Drawing.Size(44, 16);
+            this.lblDislikes.TabIndex = 4;
+            this.lblDislikes.Text = "label3";
+            // 
+            // lblPower
+            // 
+            this.lblPower.AutoSize = true;
+            this.lblPower.Location = new System.Drawing.Point(12, 142);
+            this.lblPower.Name = "lblPower";
+            this.lblPower.Size = new System.Drawing.Size(44, 16);
+            this.lblPower.TabIndex = 5;
+            this.lblPower.Text = "label4";
+            // 
+            // lblBio
+            // 
+            this.lblBio.AutoSize = true;
+            this.lblBio.Location = new System.Drawing.Point(12, 171);
+            this.lblBio.Name = "lblBio";
+            this.lblBio.Size = new System.Drawing.Size(69, 16);
+            this.lblBio.TabIndex = 6;
+            this.lblBio.Text = "Biography";
+            // 
+            // lblBiography
+            // 
+            this.lblBiography.AutoSize = true;
+            this.lblBiography.Location = new System.Drawing.Point(12, 208);
+            this.lblBiography.Name = "lblBiography";
+            this.lblBiography.Size = new System.Drawing.Size(14, 16);
+            this.lblBiography.TabIndex = 7;
+            this.lblBiography.Text = "_";
+            // 
+            // lblWiki
+            // 
+            this.lblWiki.AutoSize = true;
+            this.lblWiki.Location = new System.Drawing.Point(12, 359);
+            this.lblWiki.Name = "lblWiki";
+            this.lblWiki.Size = new System.Drawing.Size(14, 16);
+            this.lblWiki.TabIndex = 8;
+            this.lblWiki.Text = "_";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblWiki);
+            this.Controls.Add(this.lblBiography);
+            this.Controls.Add(this.lblBio);
+            this.Controls.Add(this.lblPower);
+            this.Controls.Add(this.lblDislikes);
+            this.Controls.Add(this.lblLikes);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Super Hero Database";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblLikes;
+        private System.Windows.Forms.Label lblDislikes;
+        private System.Windows.Forms.Label lblPower;
+        private System.Windows.Forms.Label lblBio;
+        private System.Windows.Forms.Label lblBiography;
+        private System.Windows.Forms.Label lblWiki;
+    }
+}
+
---
+++ Super Hero Database/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Super_Hero_Database
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, Superhero> superheroes;
+        public Form1()
+        {
+            InitializeComponent();
+            superheroes = new Dictionary<string, Superhero>
+            {
+                {"No Superhero", null},
+                {"Yondu", new Superhero("Yondu Udonta", "Yondu Udonta", "Centaurian", "Yaka Arrow")},
+                {"Thanos", new Superhero("Thanos", "Dione", "Titan", "Super strength, Invulnerability")},
+                {"Knull", new Superhero("Knull", "Unknown", "Unknown", "Symbiote control")},
+                {"Arnim Zola", new Superhero("Arnim Zola", "Arnim Zola", "Switzerland", "Genius-level intellect, Android body")},
+                {"Mister Negative", new Superhero("Mister Negative", "Martin Li", "New York City", "Darkforce manipulation")}
+            };
+            foreach (var superheroName in superheroes.Keys)
+            {
+                comboBox1.Items.Add(superheroName);
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedSuperheroName = comboBox1.SelectedItem.ToString();
+
+            if (superheroes.ContainsKey(selectedSuperheroName))
+            {
+                Superhero selectedSuperhero = superheroes[selectedSuperheroName];
+                DisplaySuperheroInfo(selectedSuperhero);
+            }
+            else
+            {
+                DisplayNoSuperheroInfo();
+            }
+        }
+        private void DisplaySuperheroInfo(Superhero superhero)
+        {
+            lblName.Text = $"Name: {superhero.Name}";
+            lblLikes.Text = $"Likes: {superhero.Likes}";
+            lblDislikes.Text = $"Dislikes: {superhero.Dislikes}";
+            lblPower.Text = $"Power: {superhero.Abilities}";
+            lblBiography.Text = $"Biography: {superhero.Biography}";
+        }
+        private void DisplayNoSuperheroInfo()
+        {
+            lblName.Text = "No superhero selected.";
+            lblLikes.Text = "";
+            lblDislikes.Text = "";
+            lblPower.Text = "";
+            lblBiography.Text = "";
+        }
+        public class Superhero
+        {
+            public string Name { get; }
+            public string AlterEgo { get; }
+            public string City { get; }
+            public string Abilities { get; }
+            public string Biography { get; }
+            public string Likes { get; }
+            public string Dislikes { get; }
+
+            public Superhero(string name, string alterEgo, string city, string abilities, string biography)
+            {
+                Name = name;
+                AlterEgo = alterEgo;
+                City = city;
+                Abilities = abilities;
+                Biography = biography;
+            }
+
+            public Superhero(string name, string alterEgo, string city, string abilities, string likes, string dislikes)
+            {
+                Name = name;
+                AlterEgo = alterEgo;
+                City = city;
+                Abilities = abilities;
+                Likes = likes;
+                Dislikes = dislikes;
+            }
+        }
+    }
+}
+
